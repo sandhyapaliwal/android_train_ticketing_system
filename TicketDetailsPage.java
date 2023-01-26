8e8208ee38709f3e665cf5ea391a72bb91d86eaa
sandhyapaliwal <100416191+sandhyapaliwal@users.noreply.github.com>
2023-01-26 23:19:05 +0530

    Add files via upload

---
+++ TicketDetailsPage.java
@@ -0,0 +1,54 @@
+
+public class TicketDetailsPage extends AppCompatActivity {
+
+    private EditText TKuserIdFld, TKtoFld, TKfromFld, TKtypeFld, TKdateFld;
+    private Button TKClearBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ticket_details_page);
+
+
+        TKuserIdFld = (EditText) findViewById(R.id.TKuserIdFld);
+        TKtoFld = (EditText) findViewById(R.id.TKtoFld);
+        TKfromFld = (EditText) findViewById(R.id.TKformFld);
+        TKtypeFld = (EditText) findViewById(R.id.TKtypeFld);
+        TKdateFld = (EditText) findViewById(R.id.TKdateFld);
+
+        TKClearBtn = (Button) findViewById(R.id.TKclearBtn);
+
+        TicketDetails details = (TicketDetails) getIntent().getSerializableExtra("ticketdetails");
+
+        if (details != null) {
+            TKuserIdFld.setText(details.getTicketId() + "");
+            TKtoFld.setText(details.getToStation());
+            TKfromFld.setText(details.getFromStation());
+            TKtypeFld.setText(details.getType());
+            TKdateFld.setText(details.getDate());
+        }
+
+        TicketDetails viewDetails = (TicketDetails) getIntent().getSerializableExtra("TicDeatils");
+
+//        final int kId=viewDetails.getTicketId();
+
+        if (viewDetails != null) {
+            TKuserIdFld.setText(viewDetails.getTicketId() + "");
+            TKtoFld.setText(viewDetails.getToStation());
+            TKfromFld.setText(viewDetails.getFromStation());
+            TKtypeFld.setText(viewDetails.getType());
+            TKdateFld.setText(viewDetails.getDate());
+        }
+
+        TKClearBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bookingLab.get(TicketDetailsPage.this).deletTicket(Integer.parseInt(TKuserIdFld.getText().toString()));
+                Toast.makeText(TicketDetailsPage.this, "Deleted Sucessfully", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        });
+
+
+    }
+}
